1946ffb06f61730b964fa6f1b4d4aeddf6a7d2ba
igasimon25 <igasimon25@gmail.com>
2026-09-06 20:52:21 +0700

    Update Dataapp.py

--- Dataapp.py
+++ Dataapp.py
@@ -720,7 +720,7 @@ if not df_tsel_agent.empty:
     </body>
     </html>
     """
-    components.html(full_html_tsel, height=550, scrolling=True)
+ components.html(full_html, height=650, scrolling=True)
 
 def generate_risk_vat_summary(df):
     df_calc = df.copy()
